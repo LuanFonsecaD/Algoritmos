15f04a5613a43eb5948ce1fafd0fd1500a870bf7
Luan Fonseca <luanjeremias2@gmail.com>
2023-02-22 09:14:37 -0300

    Commit primeiro

---
+++ area-circ.js
@@ -0,0 +1,8 @@
+console.log("Para calcular a área do círculo, informe o tamanho do raio: ");
+const raio = 3;
+const area = Math.PI * raio ** 2;
+const areaStr = area.toFixed(2);
+console.log(`A área do círculo é: ${areaStr} m².`);
+const areaNum = Number(areaStr);
+
+console.log(areaNum);
